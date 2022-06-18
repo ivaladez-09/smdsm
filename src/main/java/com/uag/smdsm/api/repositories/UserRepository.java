045b44c1b5f139671e30b91ca9846192d4da16ce
Ivan Valadez <fvaladez@infobip.com>
2022-06-17 19:14:58 -0500

    Fixing count queries to look for only the active diseases and active risk factors.

--- src/main/java/com/uag/smdsm/api/repositories/UserRepository.java
+++ src/main/java/com/uag/smdsm/api/repositories/UserRepository.java
@@ -13,14 +13,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT COUNT(*) FROM users u\n" +
             "        INNER JOIN user_disease ud ON ud.id_user=u.id\n" +
             "        INNER JOIN diseases d ON ud.id_disease=d.id\n" +
-            "        WHERE u.gender = :gender AND d.name = :disease", nativeQuery = true)
+            "        WHERE u.gender = :gender AND ud.active = 1 AND d.name = :disease", nativeQuery = true)
     Integer countByGenderAndDisease(@Param("gender") String gender,
                                     @Param("disease") String disease);
 
     @Query(value = "SELECT COUNT(*) FROM users u\n" +
             "        INNER JOIN user_risk_factor ur ON ur.id_user=u.id\n" +
             "        INNER JOIN risk_factors r ON ur.id_risk_factor=r.id\n" +
-            "        WHERE u.gender = :gender AND r.name = :riskFactor", nativeQuery = true)
+            "        WHERE u.gender = :gender AND ur.active = 1 AND r.name = :riskFactor", nativeQuery = true)
     Integer countByGenderAndRiskFactor(@Param("gender") String gender,
                                        @Param("riskFactor") String riskFactor);
 }
